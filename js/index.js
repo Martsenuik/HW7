1a6dec3d8009b453ff7fa59da7bea77651ea7aa8
Martseniuk Alina <martseniuk.av@gmail.com>
2024-12-01 13:54:44 +0200

    add

--- js/index.js
+++ js/index.js
@@ -1,41 +1,65 @@
 //*\\\\\\\\\\\\\\\\\\\ завдання 1 \\\\\\\\\\\\\\\\\\\\\\\\\
 
-const numbers = [1, 2, 3]
-numbers[1] = 10
+// const numbers = [1, 2, 3]
+// numbers[1] = 10
 
-console.log(numbers);
+// console.log(numbers);
 
 //*\\\\\\\\\\\\\\\\\\\ завдання 2 \\\\\\\\\\\\\\\\\\\\\\\\\
 
-const names = ['Igor', 'Alina', 'LIza']
-names[3] = 'Kira'
+// const names = ['Igor', 'Alina', 'LIza']
+// names[3] = 'Kira'
 
-console.log(names);
+// console.log(names);
 
 //*\\\\\\\\\\\\\\\\\\\ завдання 3 \\\\\\\\\\\\\\\\\\\\\\\\\
 
-const numbers = [10, 70, 20]
+// const numbers = [10, 70, 20, 7];
+// let sum = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+// }
+// console.log(sum);
 
+//*\\\\\\\\\\\\\\\\\\\ завдання 4 \\\\\\\\\\\\\\\\\\\\\\\\\
 
+//const array = [22, 44, 66, 88, 99];
 
+//for (const element of array) {
+//   console.log(element);
+// }
+//*\\\\\\\\\\\\\\\\\\\ завдання 5 \\\\\\\\\\\\\\\\\\\\\\\\\
 
+// const strings = ['JavaScript', 'Pyton', 'HTML', 'CSS', 'LMS'];
 
+// for (let i = 0; i < strings.length; i++) {
+//     if (strings[i].length > 5) {
+//         console.log(strings[i]);
+        
+//     }
+    
+// }
 
+//*\\\\\\\\\\\\\\\\\\\ завдання 6 \\\\\\\\\\\\\\\\\\\\\\\\\
 
+// const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let max = num[0]
+// for (let i = 1; i < num.length; i++) {
+//     if (num[i] > max) {
+//         max = num[i]
+//     } 
+// }
+// console.log(max);
 
+//*\\\\\\\\\\\\\\\\\\\ завдання 7 \\\\\\\\\\\\\\\\\\\\\\\\\
 
+// let num = [41, 26, 39, 64, 57, 16, 67, 80, 59, 100];
 
-
-
-
-
-
-
-
-
-
-
-
+// for (let i = 0; i < num.length; i ++) {
+//   if (num[i] % 2 === 0) {
+//      console.log(num[i]);
+//   }
+// }
 
 
 
